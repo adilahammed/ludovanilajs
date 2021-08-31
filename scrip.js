d9a72846094d2eca3d2a30ab8a81d1863d60fd5f
adil <adilahmmed1444@gmail.com>
2021-08-31 15:44:18 +0530

    names and score added in html

--- scrip.js
+++ scrip.js
@@ -8,37 +8,52 @@ const player=["red","green","blue","yellow"]
 let n=0
 // console.log(n);
 let color=player[n]
+document.querySelector(".nowplay").innerHTML=color
 const spin=()=>{
     const num=Math.floor(Math.random()*6)+1
     console.log(color);
     console.log(num);
+    changename(num)
+
     if(num===1){
         let re=color
         let val=""
-        
+        let mov=0
         if(color==="red"){
             val=re+red
             red++
+            mov=1
         }else if(color==="green"){
             val=re+green
             green++
+            mov=40
         }else if(color=="yellow"){
             val=re+yellow
             yellow++
+            mov=27
         }else if(color=="blue"){
             val=re+blue
             blue++
+            mov=14
         }
 
         console.log(re);
-        document.querySelector(".num1").style.backgroundColor="red"
+        document.querySelector(`.num${mov}`).style.backgroundColor=color
         document.querySelector(`.${val}`).style.backgroundColor="white"
-
+        
+     
     }
+    
     n++
     if(n===4){
         n=0
     }
     color=player[n]
     
+}
+
+
+const changename=(num)=>{
+    document.querySelector(".nowplay").innerHTML=color
+    document.querySelector(".nowscore").innerHTML=num
 }
